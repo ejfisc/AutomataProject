0c8fdd5ee13ce0db8b2801bebb86b7373df1d831
Ethan Fischer <ejfisc@protonmail.com>
2023-02-01 11:02:54 -0600

    started DFA.java, still need to implement state transitions and test that it works, but waiting for tester class to be finished

---
+++ DFA.java
@@ -0,0 +1,49 @@
+public class DFA {
+
+    int[][] transitionTable;
+    int[] finalStates;
+    int initState;
+
+    // constructor takes in a transition array and array of finalStates
+    public DFA(int[][] transitionTable, int[] finalStates, int initState) {
+        if(transitionTable == null || finalStates == null) {
+            throw new IllegalArgumentException();
+        }
+        this.transitionTable = transitionTable;
+        this.finalStates = finalStates;
+        this.initState = initState;
+        
+    }
+
+    // determine if the input string will be accepted (true) or rejected (false) by the automata
+    boolean run(String input) {
+        int currState = initState;
+
+        for(int i = 0; i < input.length(); i++) {
+            char l = input.charAt(i);
+            if(l == '0') {
+                // set currState to next state using transitionTable
+            }
+            else { // l is '1'
+                // set currState to next state using transitionTable
+            }
+        }
+        
+        // check if the currState is a final state
+        if(contains(finalStates, currState))
+            return true;
+        else
+            return false;
+    }
+
+    public static boolean contains(final int[] arr, final int key) {
+        for (int element : arr) {
+            if(element == key) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
+
+
