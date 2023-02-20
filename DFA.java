e0eaa7e1686c8536e2efe88c2cf27e9e585e92c1
Ethan Fischer <ejfisc@protonmail.com>
2023-02-20 11:37:23 -0600

    started work on part 2 of the project, added the DFA operation functions which still need to be written, finished the complement and credits functions

--- DFA.java
+++ DFA.java
@@ -1,7 +1,15 @@
-/* Class constructs a DFA given sufficient data, 
- * and is able to determine whether to accept
- * or reject a string given the DFA construction. 
- */
+/*************************************************************
+ * DFA Class
+ * 
+ * Given a transition table, and a list of final states, 
+ * a Deterministic Finite Automata with various operations 
+ * is created.
+ * 
+ * Credits: Ethan Fischer, John Lawler, and Luke Wells
+ *************************************************************/
+
+import java.util.ArrayList;
+
 public class DFA {
 
     private int[][] transitionTable; // transitionTable[currentState][input] = nextState
@@ -17,11 +25,11 @@ public class DFA {
         this.finalStates = finalStates;
         initState = 0; // agreed to default to 0 in class
     }
-
+    
     // determine if the input string will be accepted (true) or rejected (false) by the automata
     public boolean run(String testStr) {
         int currState = initState;
-
+        
         // loop through string one by one
         for(int i = 0; i < testStr.length(); i++) {
             char ch = testStr.charAt(i);
@@ -31,13 +39,53 @@ public class DFA {
         
         // check if the currState is a final state
         if(contains(finalStates, currState))
-            return true;
+        return true;
         else
-            return false;
+        return false;
     }
+    
+    // creates a DFA that accepts the union of M1 and M2
+    public static DFA union(DFA m1, DFA m2) {
+        return m1;
+    }
+    
+    // creates a DFA that accepts the intersection of M1 and M2
+    public static DFA intersection(DFA m1, DFA m2) {
+        return m1;
+    }
+    
+    // creates a DFA that accepts the set difference of M1 and M2 (set of strings accepted by M1 but not M2)
+    public static DFA difference(DFA m1, DFA m2) {
+        return m1;
+    }
+    
+    // creates a DFA that is the complement of the given DFA (swap the final and non-final states)
+    public static DFA complement(DFA m) {
+        ArrayList<Integer> temp = new ArrayList<>(); // used for creating the new final states list
 
+        // loop through states in m.transitionTable
+        for(int i = 0; i < m.transitionTable.length; i++) {
+            // if the current state is not in m.finalStates, add that state to the list of final states for our new DFA 
+            if(!contains(m.finalStates, i)) {
+                temp.add(i);
+            }
+        }
+
+        int[] finStates = temp.stream().mapToInt(i -> i).toArray(); // convert the Integer ArrayList 'temp' to primitive int array
+
+        DFA complement = new DFA(m.transitionTable, finStates); // when creating the complement of a DFA, the transition table remains the same, so we can use m.transitionTable here
+
+        return complement;
+    }
+    
+    // credits function, prints out names of the group members (so that the grader can easily see who contributed while running the Tester class)
+    public static String credits() {
+        String credits = "Credits: Ethan Fischer, John Lawler, Luke wells";
+        return credits;
+    }
+    
     // helper method to determine if the current state is a final state
-    boolean contains(final int[] arr, final int key) {
+    static boolean contains(final int[] arr, final int key) {
         for (int element : arr) {
             if(element == key) {
                 return true;
