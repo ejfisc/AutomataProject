55736de04d13f28c060795ca7afea3488eab61c3
John <afingerintheeye@gmail.com>
2023-02-22 19:59:05 -0600

    fixed union, intersection, combineTables
    also updated version of tester file

--- DFA.java
+++ DFA.java
@@ -53,80 +53,67 @@ public class DFA {
         int m2State = 0;
         int m1State = 0;
         for(int i = 0; i < unionTable.length; i++) {
-            m2State = i % m1.transitionTable.length;
-            m1State = (i - m2State) / m1.transitionTable.length;
+        	m1State = i / m2.transitionTable.length;
+            m2State = i % m2.transitionTable.length;
+            
 
             if(contains(m1.finalStates, m1State) || contains(m2.finalStates, m2State)) {
                 temp.add(i);
             }
         }
 
-        int[] finStates = temp.stream().mapToInt(i -> i).toArray(); // convert the Integer ArrayList 'temp' to primitive int array
-        DFA union = new DFA(unionTable, finStates);
+        return new DFA(unionTable, temp.stream().mapToInt(i -> i).toArray());
+    } //*/
 
-        return union;
-    }
-
-    // creates a DFA that accepts the intersection of M1 and M2
     public static DFA intersection(DFA m1, DFA m2) {
-        
-        ArrayList<Integer> temp = new ArrayList<>(); // used for creating the new final states list
 
-        int[][] transTable = combineTransitionTables(m1, m2);
+        // create a new array of final states that contains the intersection of the final states of the two input DFAs
+        ArrayList<Integer> newFinalStates = new ArrayList<Integer>();
         
-        int m2State = 0;
-        int m1State = 0;
-        for(int i = 0; i < transTable.length; i++) {
-            // use the next intersection state = (next m1 state * num of m1 states) + next m2 state to derive which m1 state and m2 state the i state represents
-            m2State = i % m1.transitionTable.length;
-            m1State = (i - m2State) / m1.transitionTable.length;
-
-            // because this is intersection, check if both of those states are considered final states for their respective DFAs
-            if(contains(m1.finalStates, m1State) && contains(m2.finalStates, m2State)) {
-                temp.add(i); // this is a final state
+        // add the intersection of the final states between the DFAs
+        for (int i = 0; i < m1.finalStates.length; i++) {
+            for (int j = 0; j < m2.finalStates.length; j++) {
+                newFinalStates.add(m1.finalStates[i] * m2.transitionTable.length + m2.finalStates[j]);
             }
         }
 
-        int[] finStates = temp.stream().mapToInt(i -> i).toArray(); // convert the Integer ArrayList 'temp' to primitive int array
-
-        DFA intersection = new DFA(transTable, finStates);
-
-        return intersection;
+        // return a DFA of the combined transition table, and an int[] of the new final states
+        return new DFA(combineTransitionTables(m1, m2), newFinalStates.stream().mapToInt(i -> i).toArray());
     }
-    
+     
     // creates a DFA that accepts the set difference of M1 and M2 (set of strings accepted by M1 but not M2)
     public static DFA difference(DFA m1, DFA m2) {
-    	return intersection(m1, complement(m2));
+    	return intersection(m1, complement(m2));	// difference is equivalent to (m1 (intersection) !m2)
     }
     
     // creates a DFA that is the complement of the given DFA (swap the final and non-final states)
     public static DFA complement(DFA m) {
+    	
         ArrayList<Integer> temp = new ArrayList<>(); // used for creating the new final states list
 
         // loop through states in m.transitionTable`
         for(int i = 0; i < m.transitionTable.length; i++) {
+        	
             // if the current state is not in m.finalStates, add that state to the list of final states for our new DFA 
             if(!contains(m.finalStates, i)) {
                 temp.add(i);
             }
         }
-
-        int[] finStates = temp.stream().mapToInt(i -> i).toArray(); // convert the Integer ArrayList 'temp' to primitive int array
-
-        DFA complement = new DFA(m.transitionTable, finStates); // when creating the complement of a DFA, the transition table remains the same, so we can use m.transitionTable here
-
-        return complement;
+        
+        // return new DFA of the transition table, and an int[] of the complement final states
+        return new DFA(m.transitionTable, temp.stream().mapToInt(i -> i).toArray());
     }
     
     // credits function, prints out names of the group members (so that the grader can easily see who contributed while running the Tester class)
     public static String credits() {
-        String credits = "Credits: Ethan Fischer, John Lawler, Luke Wells";
-        return credits;
+        return "Credits: Ethan Fischer, John Lawler, Luke Wells";
     }
     
     // helper method to determine if the current state is a final state
     private static boolean contains(final int[] arr, final int key) {
-        for (int element : arr) {
+        
+    	// check to see if any element in the array matches key, if so return true
+    	for (int element : arr) {
             if(element == key) {
                 return true;
             }
@@ -134,48 +121,32 @@ public class DFA {
         return false;
     }
 
-    private static int[][] combineTransitionTables(DFA m1, DFA m2) {
-        int numOfM1States = m1.transitionTable.length;
-        int numOfM2States = m2.transitionTable.length;
-        int[][] intersectionTransitions = new int[numOfM1States*numOfM2States][2]; // new transition table
+    public static int[][] combineTransitionTables(DFA m1, DFA m2) {
+    	
+    	// create a new table that is the size of the combined transition table
+        int[][] combinedTable = new int[m1.transitionTable.length * m2.transitionTable.length][2];
 
-        /* Consider two 3 state DFAs, these would be the new states:
-         * 0 -> m1_0, m2_0
-         * 1 -> m1_0, m2_1
-         * 2 -> m1_0, m2_2
-         * 3 -> m1_1, m2_0
-         * 4 -> m1_1, m2_1
-         * 5 -> m1_1, m2_2
-         * 6 -> m1_2, m2_0
-         * 7 -> m1_2, m2_1
-         * 8 -> m1_2, m2_2
-         * to determine which state in the intersection state table to put a transition, we use
-         next intersect state = (next m1 state * # of m1 states) + next m2 state */
-         
-         int nxtM1_0State = 0; 
-         int nxtM1_1State = 0;
-         int nxtM2State = 0;
-         for(int i = 0; i < intersectionTransitions.length; i++) {
-             for(int j = 0; j < numOfM1States; j++) {
-                 /* I split the next M1 state into two because it allowed me to take the next m1 state computation out of the m2 loop so it's more efficient,
-                 But I needed to be able to remember what the next state was for both inputs. */
-                 nxtM1_0State = m1.transitionTable[j][0];
-                 nxtM1_1State = m1.transitionTable[j][1];
-                 for(int k = 0; k < numOfM2States; k++) {
-                     // 0 transition
-                     nxtM2State = m2.transitionTable[k][0];
-                     intersectionTransitions[i][0] = (nxtM1_0State * numOfM1States) + nxtM2State;
-                     // 1 transition
-                     nxtM2State = m2.transitionTable[k][1];
-                     intersectionTransitions[i][1] = (nxtM1_1State * numOfM1States) + nxtM2State;
-                 }
-             }
-         }
+        // for every spot on each transition table
+        for (int i = 0; i < m1.transitionTable.length * m2.transitionTable.length; i++) {
+        	
+        	// for every character in the alphabet
+            for (int j = 0; j < 2; j++) {
+            	
+            	// set next state appropriately
+                int nextState1 = m1.transitionTable[i / m2.transitionTable.length][j];
+                int nextState2 = m2.transitionTable[i % m2.transitionTable.length][j];
+                
+                // create the new combined state and add it to the table
+                int newState = nextState1 * m2.transitionTable.length + nextState2;
+                combinedTable[i][j] = newState;
+            }
+        }
 
-         return intersectionTransitions;
-    }
+        // return the combined transition table
+        return combinedTable;
+        
 
-
+    } //*/
 }
 
 
--- Tester.java
+++ Tester.java
@@ -1,202 +1,207 @@
 /**
  * Tests DFA class to generate score
  */
+import java.util.ArrayList;
 public class Tester {
 
-	/**
-	 * Executes tests against the DFA class and produces a final score.
-	 *
-	 * @param args not used
-	 */
-	public static void main(String[] args) {
+    /**
+     * Executes tests against the DFA class and produces a final score.
+     *
+     * @param args not used
+     */
+	public static ArrayList<String> failed = new ArrayList<String>();
+    public static void main(String[] args) {
 
-		/*
-		 * Answer sheet:
-		 * DFA one should accept: 000010, 0010
-		 * DFA two should accept: 1100100
-		 * DFA three should accept: 1110001
-		 */
+        /*
+         * Answer sheet:
+         * DFA one should accept: 000010, 0010
+         * DFA two should accept: 1100100
+         * DFA three should accept: 1110001
+         */
 
-		String[] testStrings = {"000010", "1110001", "0010", "1100100", "", "0010010110011"};
+        String[] testStrings = {"000010", "1110001", "0010", "1100100", "", "0010010110011"};
 
-		// A DFA that will check if a string ends in "10"
-		int[][] testOneTransitionTable = new int[3][2];
+        // A DFA that will check if a string ends in "10"
+        int[][] testOneTransitionTable = new int[3][2];
 
-		// format is as follows
-		// transitionTable[currentState][currentInput] = nextState
-		testOneTransitionTable[0][1] = 1; // at state 0, if 1 is input, transition to state 1
-		testOneTransitionTable[0][0] = 0;
-		testOneTransitionTable[1][0] = 2;
-		testOneTransitionTable[1][1] = 1;
-		testOneTransitionTable[2][0] = 0;
-		testOneTransitionTable[2][1] = 1;
-		int[] testOneFinalStates = {2};
+        // format is as follows
+        // transitionTable[currentState][currentInput] = nextState
+        testOneTransitionTable[0][1] = 1; // at state 0, if 1 is input, transition to state 1
+        testOneTransitionTable[0][0] = 0;
+        testOneTransitionTable[1][0] = 2;
+        testOneTransitionTable[1][1] = 1;
+        testOneTransitionTable[2][0] = 0;
+        testOneTransitionTable[2][1] = 1;
+        int[] testOneFinalStates = {2};
 
-		// A DFA that will check if a string has an odd number of "1" and ends in at
-		// least two "0"
-		int[][] testTwoTransitionTable = new int[4][2];
-		testTwoTransitionTable[0][1] = 1;
-		testTwoTransitionTable[0][0] = testTwoTransitionTable[1][1] = testTwoTransitionTable[2][1] = testTwoTransitionTable[3][1] = 0;
-		testTwoTransitionTable[1][0] = 2;
-		testTwoTransitionTable[2][0] = testTwoTransitionTable[3][0] = 3;
-		int[] testTwoFinalStates = {3};
+        // A DFA that will check if a string has an odd number of "1" and ends in at
+        // least two "0"
+        int[][] testTwoTransitionTable = new int[4][2];
+        testTwoTransitionTable[0][1] = 1;
+        testTwoTransitionTable[0][0] = testTwoTransitionTable[1][1] = testTwoTransitionTable[2][1] = testTwoTransitionTable[3][1] = 0;
+        testTwoTransitionTable[1][0] = 2;
+        testTwoTransitionTable[2][0] = testTwoTransitionTable[3][0] = 3;
+        int[] testTwoFinalStates = {3};
 
-		// A DFA that will check if a string has an even number of "1" and an odd
-		// number of "0"
-		int[][] testThreeTransitionTable = new int[4][2];
-		testThreeTransitionTable[0][0] = 1;
-		testThreeTransitionTable[0][1] = 2;
-		testThreeTransitionTable[1][0] = 0;
-		testThreeTransitionTable[1][1] = 3;
-		testThreeTransitionTable[2][0] = 3;
-		testThreeTransitionTable[2][1] = 0;
-		testThreeTransitionTable[3][0] = 2;
-		testThreeTransitionTable[3][1] = 1;
-		int[] testThreeFinalStates = {1};
+        // A DFA that will check if a string has an even number of "1" and an odd
+        // number of "0"
+        int[][] testThreeTransitionTable = new int[4][2];
+        testThreeTransitionTable[0][0] = 1;
+        testThreeTransitionTable[0][1] = 2;
+        testThreeTransitionTable[1][0] = 0;
+        testThreeTransitionTable[1][1] = 3;
+        testThreeTransitionTable[2][0] = 3;
+        testThreeTransitionTable[2][1] = 0;
+        testThreeTransitionTable[3][0] = 2;
+        testThreeTransitionTable[3][1] = 1;
+        int[] testThreeFinalStates = {1};
 
-		// A DFA that will accept strings whose second to last symbol is '0'
-		int[][] testFourTransitionTable = new int[4][2];
-		testFourTransitionTable[0][0] = 1;
-		testFourTransitionTable[0][1] = 0;
-		testFourTransitionTable[1][0] = 3;
-		testFourTransitionTable[1][1] = 2;
-		testFourTransitionTable[2][0] = 1;
-		testFourTransitionTable[2][1] = 0;
-		testFourTransitionTable[3][0] = 3;
-		testFourTransitionTable[3][1] = 2;
-		int[] testFourFinalStates = {2, 3};
+        // A DFA that will accept strings whose second to last symbol is '0'
+        int[][] testFourTransitionTable = new int[4][2];
+        testFourTransitionTable[0][0] = 1;
+        testFourTransitionTable[0][1] = 0;
+        testFourTransitionTable[1][0] = 3;
+        testFourTransitionTable[1][1] = 2;
+        testFourTransitionTable[2][0] = 1;
+        testFourTransitionTable[2][1] = 0;
+        testFourTransitionTable[3][0] = 3;
+        testFourTransitionTable[3][1] = 2;
+        int[] testFourFinalStates = {2, 3};
 
-		// A DFA that will only accept empty strings
-		int[][] testFiveTransitionTable = new int[2][2];
-		testFiveTransitionTable[0][0] = 1;
-		testFiveTransitionTable[0][1] = 1;
-		testFiveTransitionTable[1][0] = 1;
-		testFiveTransitionTable[1][1] = 1;
-		int[] testFiveFinalStates = {0};
+        // A DFA that will only accept empty strings
+        int[][] testFiveTransitionTable = new int[2][2];
+        testFiveTransitionTable[0][0] = 1;
+        testFiveTransitionTable[0][1] = 1;
+        testFiveTransitionTable[1][0] = 1;
+        testFiveTransitionTable[1][1] = 1;
+        int[] testFiveFinalStates = {0};
+        
+        //A DFA that accepts strings that end in 1
+        int[][] testSixTransitionTable = new int[2][2];
+        testSixTransitionTable[0][0] = 0;
+        testSixTransitionTable[0][1] = 1;
+        testSixTransitionTable[1][0] = 0;
+        testSixTransitionTable[1][1] = 1;
+        int[] testSixFinalStates = {1};
 
-		//A DFA that accepts strings that end in 1
-		int[][] testSixTransitionTable = new int[2][2];
-		testSixTransitionTable[0][0] = 0;
-		testSixTransitionTable[0][1] = 1;
-		testSixTransitionTable[1][0] = 0;
-		testSixTransitionTable[1][1] = 1;
-		int[] testSixFinalStates = {1};
+        DFA one = new DFA(testOneTransitionTable, testOneFinalStates);
+        DFA two = new DFA(testTwoTransitionTable, testTwoFinalStates);
+        DFA three = new DFA(testThreeTransitionTable, testThreeFinalStates);
+        DFA four = new DFA(testFourTransitionTable, testFourFinalStates);
+        DFA five = new DFA(testFiveTransitionTable, testFiveFinalStates);
+        DFA six = new DFA(testSixTransitionTable, testSixFinalStates);
 
-		DFA one = new DFA(testOneTransitionTable, testOneFinalStates);
-		DFA two = new DFA(testTwoTransitionTable, testTwoFinalStates);
-		DFA three = new DFA(testThreeTransitionTable, testThreeFinalStates);
-		DFA four = new DFA(testFourTransitionTable, testFourFinalStates);
-		DFA five = new DFA(testFiveTransitionTable, testFiveFinalStates);
-		DFA six = new DFA(testSixTransitionTable, testSixFinalStates);
+        int testsPassed = 0;
+        int testsRan = 0;
 
-		int testsPassed = 0;
-		int testsRan = 0;
+        // Test each
+        testsPassed += test(one, "one", testStrings, new boolean[]{true, false, true, false, false, false});
+        testsRan += testStrings.length;
 
-		// Test each
-		testsPassed += test(one, "one", testStrings, new boolean[]{true, false, true, false, false, false});
-		testsRan += testStrings.length;
+        testsPassed += test(two, "two", testStrings, new boolean[]{false, false, false, true, false, false});
+        testsRan += testStrings.length;
 
-		testsPassed += test(two, "two", testStrings, new boolean[]{false, false, false, true, false, false});
-		testsRan += testStrings.length;
+        testsPassed += test(three, "three", testStrings, new boolean[]{false, true, false, false, false, true});
+        testsRan += testStrings.length;
 
-		testsPassed += test(three, "three", testStrings, new boolean[]{false, true, false, false, false, true});
-		testsRan += testStrings.length;
+        testsPassed += test(four, "four", testStrings, new boolean[]{false, true, false, true, false, false});
+        testsRan += testStrings.length;
 
-		testsPassed += test(four, "four", testStrings, new boolean[]{false, true, false, true, false, false});
-		testsRan += testStrings.length;
+        testsPassed += test(five, "five", testStrings, new boolean[]{false, false, false, false, true, false});
+        testsRan += testStrings.length;
+        
+        testsPassed += test(six, "six", testStrings, new boolean[]{false, true, false, false, false, true});
+        testsRan += testStrings.length;
+        
+        //Testing the union, intersection, difference, and complement
+        testsPassed += test(DFA.union(one, two), "one OR two", testStrings, new boolean[]{true, false, true, true, false, false});
+        testsRan += testStrings.length;
+        
+        testsPassed += test(DFA.union(one, five), "one OR five", testStrings, new boolean[]{true, false, true, false, true, false});
+        testsRan += testStrings.length;
+        
+        testsPassed += test(DFA.union(two, four), "two OR four", testStrings, new boolean[]{false, true, false, true, false, false});
+        testsRan += testStrings.length;
+        
+        testsPassed += test(DFA.union(five, six), "five OR six", testStrings, new boolean[]{false, true, false, false, true, true});
+        testsRan += testStrings.length;
+        
+        testsPassed += test(DFA.intersection(three, four), "three AND four", testStrings, new boolean[]{false, true, false, false, false, false});
+        testsRan += testStrings.length;
+        
+        testsPassed += test(DFA.intersection(three, six), "three AND six", testStrings, new boolean[]{false, true, false, false, false, true});
+        testsRan += testStrings.length;
+        
+        testsPassed += test(DFA.intersection(four, six), "four AND six", testStrings, new boolean[]{false, true, false, false, false, false});
+        testsRan += testStrings.length;
+        
+        testsPassed += test(DFA.intersection(one, five), "one AND five", testStrings, new boolean[]{false, false, false, false, false, false});
+        testsRan += testStrings.length;
+        
+        testsPassed += test(DFA.difference(three, four), "three - four", testStrings, new boolean[]{false, false, false, false, false, true});
+        testsRan += testStrings.length;
+        
+        testsPassed += test(DFA.difference(four, three), "four - three", testStrings, new boolean[]{false, false, false, true, false, false});
+        testsRan += testStrings.length;
+        
+        testsPassed += test(DFA.difference(two, one), "two - one", testStrings, new boolean[]{false, false, false, true, false, false});
+        testsRan += testStrings.length;
+        
+        testsPassed += test(DFA.difference(four, two), "four - two", testStrings, new boolean[]{false, true, false, false, false, false});
+        testsRan += testStrings.length;
+        
+        testsPassed += test(DFA.complement(one), "Complement of one", testStrings, new boolean[]{false, true, false, true, true, true});
+        testsRan += testStrings.length;
+        
+        testsPassed += test(DFA.complement(two), "Complement of two", testStrings, new boolean[]{true, true, true, false, true, true});
+        testsRan += testStrings.length;
+        
+        testsPassed += test(DFA.complement(three), "Complement of three", testStrings, new boolean[]{true, false, true, true, true, false});
+        testsRan += testStrings.length;
+        
+        testsPassed += test(DFA.complement(four), "Complement of four", testStrings, new boolean[]{true, false, true, false, true, true});
+        testsRan += testStrings.length;
+        
+        testsPassed += test(DFA.complement(five), "Complement of five", testStrings, new boolean[]{true, true, true, true, false, true});
+        testsRan += testStrings.length;
+        
+        testsPassed += test(DFA.complement(six), "Complement of six", testStrings, new boolean[]{true, false, true, true, true, false});
+        testsRan += testStrings.length;
+        
 
-		testsPassed += test(five, "five", testStrings, new boolean[]{false, false, false, false, true, false});
-		testsRan += testStrings.length;
-
-		testsPassed += test(six, "six", testStrings, new boolean[]{false, true, false, false, false, true});
-		testsRan += testStrings.length;
-
-		//Testing the union, intersection, difference, and complement
-		// Union Fails Most Tests
-		testsPassed += test(DFA.union(one, two), "DFA one OR two", testStrings, new boolean[]{true, false, true, true, false, false});
-		testsRan += testStrings.length;
-
-		testsPassed += test(DFA.union(one, five), "DFA one OR five", testStrings, new boolean[]{true, false, true, false, true, false});
-		testsRan += testStrings.length;
-
-		testsPassed += test(DFA.union(two, four), "DFA two OR four", testStrings, new boolean[]{false, true, false, true, false, false});
-		testsRan += testStrings.length;
-
-		testsPassed += test(DFA.intersection(three, four), "DFA three AND four", testStrings, new boolean[]{false, true, false, false, false, false});
-		testsRan += testStrings.length;
-
-		// Intersection Completely Breaks Here \/
-		testsPassed += test(DFA.intersection(three, six), "DFA three AND six", testStrings, new boolean[]{false, true, false, false, false, true});
-		testsRan += testStrings.length;
-
-		testsPassed += test(DFA.intersection(four, six), "DFA four AND six", testStrings, new boolean[]{false, true, false, false, false, false});
-		testsRan += testStrings.length;
-
-		testsPassed += test(DFA.difference(three, four), "DFA three - four", testStrings, new boolean[]{false, false, false, false, false, true});
-		testsRan += testStrings.length;
-
-		testsPassed += test(DFA.complement(one), "Complement of DFA one", testStrings, new boolean[]{false, true, false, true, true, true});
-		testsRan += testStrings.length;
-
-		testsPassed += test(DFA.complement(five), "Complement of DFA five", testStrings, new boolean[]{true, true, true, true, false, true});
-		testsRan += testStrings.length;
-
-		//System.out.println(DFA.complement(five).getFinalStates());
-
-		System.out.print("\n\nYou got " + testsPassed + "/" + testsRan + " correct.\n\n");
-
-        /*System.out.println(three.getFinalStates());
-        System.out.println(DFA.complement(three).getFinalStates());
-        System.out.println(DFA.union(one, three).getFinalStates());
-        System.out.println(DFA.intersection(one, three).getFinalStates());
-        System.out.println(DFA.intersection(one, three).run("00111000010"));
-        System.out.println(DFA.intersection(three, four).run("0011001000100"));
-        System.out.println(DFA.difference(one, three).run("001100100010"));*/
-
-	}
-
-	/**
-	 * Tests a DFA using a set of testStrings and compares the results to desired results to return a score.
-	 *
-	 * @param dfa                DFA to be tested
-	 * @param testName           name of test ex: "one"
-	 * @param testStrings        array of strings to be tested
-	 * @param desiredTestResults booleans corresponding to test strings indicating whether test is expected to pass or fail
-	 * @return number of tests passed
-	 */
-	public static int test(DFA dfa, String testName, String[] testStrings, boolean[] desiredTestResults) {
-
-		int testsPassed = 0;
-
-		for (int i = 0; i < testStrings.length; i++) {
-
-			System.out.printf("DFA %5s with input %10s ", testName, testStrings[i]);
-
-			if (dfa.run(testStrings[i])) {
-
-				System.out.print("Accepted");
-
-				if (desiredTestResults[i]) {
-					System.out.println(" \t(passed test)");
-					testsPassed++;
-				} else {
-					System.out.println(" \t(failed test)");
-				}
-
-			} else {
-
-				System.out.print("Rejected");
-
-				if (!desiredTestResults[i]) {
-					System.out.println(" \t(passed test)");
-					testsPassed++;
-				} else {
-					System.out.println(" \t(failed test)");
-				}
-
-			}
+		System.out.println();
+		for(int i = 0; i < failed.size(); i++)
+		{
+			System.out.print(failed.get(i));
 		}
+        System.out.println("\nYou got " + testsPassed + "/" + testsRan + " correct.");
+		System.out.println("Credits: " + DFA.credits());
+    }
 
-		return testsPassed;
-	}
+    /**
+     * Tests a DFA using a set of testStrings and compares the results to desired results to return a score.
+     *
+     * @param dfa                DFA to be tested
+     * @param testName           name of test ex: "one"
+     * @param testStrings        array of strings to be tested
+     * @param desiredTestResults booleans corresponding to test strings indicating whether test is expected to pass or fail
+     * @return number of tests passed
+     */
+    public static int test(DFA dfa, String testName, String[] testStrings, boolean[] desiredTestResults) {
+
+        int testsPassed = 0;
+
+        for (int i = 0; i < testStrings.length; i++) {
+			if(dfa.run(testStrings[i]) != desiredTestResults[i]) {
+				if(desiredTestResults[i])
+					failed.add(testName + " with input: [" + testStrings[i] + "] returned FALSE. Expected TRUE\n");
+				else
+					failed.add(testName + " with input: [" + testStrings[i] + "] returned TRUE. Expected FALSE\n");
+			}
+			else
+				testsPassed++;
+        }
+
+        return testsPassed;
+    }
 }
