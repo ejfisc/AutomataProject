4dfb853e594ceeca942e093340d9d3cb89267e0f
Ethan Fischer <ejfisc@protonmail.com>
2023-03-07 01:30:36 -0600

    finished the base for our functions, just need to debug

--- DFA.java
+++ DFA.java
@@ -9,6 +9,7 @@
  *************************************************************/
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class DFA {
 
@@ -24,6 +25,11 @@ public class DFA {
         this.transitionTable = transitionTable;
         this.finalStates = finalStates;
         initState = 0; // agreed to default to 0 in class
+    }
+
+    // credits function, prints out names of the group members (so that the grader can easily see who contributed while running the Tester class)
+    public static String credits() {
+        return "Credits: Ethan Fischer, John Lawler, Luke Wells";
     }
     
     // determine if the input string will be accepted (true) or rejected (false) by the automata
@@ -104,16 +110,127 @@ public class DFA {
         // return new DFA of the transition table, and an int[] of the complement final states
         return new DFA(m.transitionTable, temp.stream().mapToInt(i -> i).toArray());
     }
-    
-    // credits function, prints out names of the group members (so that the grader can easily see who contributed while running the Tester class)
-    public static String credits() {
-        return "Credits: Ethan Fischer, John Lawler, Luke Wells";
+
+    // determines if the language of the DFA is empty
+    public boolean isEmptyLanguage() {
+        boolean[] visited = new boolean[transitionTable.length];
+
+        dfs(visited, initState); // perform depth first search from init state, if a final state has been visited, the language is not empty
+
+        for(int state : finalStates) {
+            if(visited[state]) {
+                return false; // a final state is reachable, so the language is not empty
+            }
+        }
+
+        return true; // no final state is reachable, so the language is empty
     }
+
+    // determines if the language of the DFA is universal
+    public boolean isUniversalLanguage() {
+        // check if all binary bit strings of the same length as the transition table are accepted by the DFA
+        int inputLength = transitionTable[0].length;
+        for(int i = 0; i < (i << inputLength); i++) { // iterate over all binary bit strings
+            int state = 0; // start at the init state
+            for(int j = 0; j < inputLength; j++) {
+                int input = (i >> j) & 1; // extract the jth bit of i
+                state = transitionTable[state][input]; // transition to the next state
+            }
+            if(!contains(finalStates, state)) {
+                return false; // if the final state is not reached, the string is not accepted, so the language is not universal
+            }
+        }
+        return true; // all binary bit strings of the same length as the transition table are accepted, so the langauge is universal
+    }
+
+    // checks for loops in the DFA to determine if the language is infinite or not
+    public boolean isInfinite() {
+        // Perform depth-first search from each state to detect loops of any length
+        for (int i = 0; i < transitionTable.length; i++) {
+            boolean[] visited = new boolean[transitionTable.length];
+            ArrayList<Integer> path = new ArrayList<>();
+            if (dfsPath(i, i, visited, path)) {
+                // If a loop was found that leads to a final state, the language is infinite
+                if (contains(finalStates, path.get(path.size() - 1))) {
+                    return true;
+                }
+            }
+        }
+
+        // If no loops of any length were found, the language is finite
+        return false;
+    }
+
+    // checks if the language for the given DFA is equivalent to the language of this DFA
+    @Override
+    public boolean equals(Object obj) {
+        // check if obj is a valid DFA
+        if(obj == null) return false;
+        if(obj.getClass() != this.getClass()) return false;
+        final DFA m2 = (DFA) obj; // cast the Object into a DFA
+        
+        // Check if the number of states is the same
+        if (this.transitionTable.length != m2.transitionTable.length) {
+            return false;
+        }
+        
+        // Check if the number of final states is the same
+        if (this.finalStates.length != m2.finalStates.length) {
+            return false;
+        }
+        
+        // Check if the final states are the same
+        for (int i = 0; i < this.finalStates.length; i++) {
+            if (this.finalStates[i] != m2.finalStates[i]) {
+                return false;
+            }
+        }
+        
+        // Check if the transition table is the same
+        for (int i = 0; i < this.transitionTable.length; i++) {
+            for (int j = 0; j < this.transitionTable[i].length; j++) {
+                if (this.transitionTable[i][j] != m2.transitionTable[i][j]) {
+                    return false;
+                }
+            }
+        }
+        // If all checks pass, the DFAs are equivalent
+        return true;
+    }
+
+    // determines if the language of this DFA is a subset of the given DFA
+    public boolean isSubsetOf(DFA m2) {
+        // Check if every string accepted by this DFA is also accepted by m2
+        for (int i = 0; i < this.transitionTable.length; i++) {
+            // Check if the current state is a final state in both DFAs or not
+            boolean isFinalInM1 = contains(this.finalStates, i);
+            boolean isFinalInM2 = contains(m2.finalStates, i);
+            if (isFinalInM1 && !isFinalInM2) {
+                // If this DFA has a final state that m2 doesn't, it's not a subset
+                return false;
+            }
+            
+            // Check if the transitions are the same for each input symbol
+            for (int j = 0; j < this.transitionTable[i].length; j++) {
+                int nextStateInThis = this.transitionTable[i][j];
+                int nextStateInM2 = m2.transitionTable[i][j];
+                if (nextStateInThis != nextStateInM2) {
+                    // If the next state is different in the two DFAs, it's not a subset
+                    return false;
+                }
+            }
+        }
+    
+        // If we've checked every state and transition and haven't returned false, this DFA is a subset
+        return true;
+    }
+    
+    /*-----------------------------------------------------
+     * Private Helper Methods
+     *-----------------------------------------------------*/
     
     // helper method to determine if the current state is a final state
     private static boolean contains(final int[] arr, final int key) {
-        
-    	// check to see if any element in the array matches key, if so return true
     	for (int element : arr) {
             if(element == key) {
                 return true;
@@ -123,7 +240,7 @@ public class DFA {
     }
 
     // combines the transition tables of two DFAs for the union or intersection of the two
-    public static int[][] combineTransitionTables(DFA m1, DFA m2) {
+    private static int[][] combineTransitionTables(DFA m1, DFA m2) {
     	
     	// create a new table that is the size of the combined transition table
         int[][] combinedTable = new int[m1.transitionTable.length * m2.transitionTable.length][2];
@@ -146,9 +263,37 @@ public class DFA {
 
         // return the combined transition table
         return combinedTable;
-        
-
     } 
+
+    // depth first search helper function for isEmptyLanguage
+    private void dfs(boolean[] visited, int state) {
+        visited[state] = true;
+
+        for(int input = 0; input < transitionTable[state].length; input++) {
+            int nextState = transitionTable[state][input];
+            if(!visited[nextState]) {
+                dfs(visited, nextState);
+            }
+        }
+    }
+
+    // depth first search helper function for finding a loop for isInfinite
+    private boolean dfsPath(int startState, int currentState, boolean[] visited, ArrayList<Integer> path) {
+        visited[currentState] = true;
+        path.add(currentState);
+        for (int j = 0; j < 2; j++) { // Assume binary alphabet
+            int nextState = transitionTable[currentState][j];
+            if (nextState == startState && visited[startState] && path.size() > 1) {
+                return true; // Loop found
+            }
+            if (!visited[nextState] && dfsPath(startState, nextState, visited, path)) {
+                return true; // Loop found further down the path
+            }
+        }
+        path.remove(path.size() - 1);
+        visited[currentState] = false;
+        return false;
+    }
 }
 
 
